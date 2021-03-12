80ed208e42366978cbc966f7ca3511d4e27cd6c8
Mahdi <mahdiayyad74@yahoo.com>
2021-03-12 11:48:23 +0200

    modified readme and added files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let mySidenav = document.getElementById("mySidenav");
+
+function openNav() {
+  mySidenav.style.width = "250px";
+}
+
+function closeNav() {
+  mySidenav.style.width = "0";
+}
