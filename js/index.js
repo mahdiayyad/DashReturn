5e96e6931ddfdb73f56e87a65baa834bd5563670
Mahdi <mahdiayyad74@yahoo.com>
2021-03-12 16:23:50 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 "use strict";
 let mySidenav = document.getElementById("mySidenav");
+let modal = document.getElementById("myModal");
+let readMoreBtn = document.getElementById("readMoreBtn");
+let span = document.getElementsByClassName("close")[0];
 
 function openNav() {
   mySidenav.style.width = "250px";
@@ -9,8 +12,22 @@ function closeNav() {
   mySidenav.style.width = "0";
 }
 
-const x = 10;
-const y = 20;
+// When the user clicks the button, open the modal
+readMoreBtn.onclick = function () {
+  modal.style.display = "block";
+  modal.classList.add("showModal");
+};
 
-const re = x + y;
-console.log("Hello World!");
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+  modal.classList.remove("showModal");
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    modal.classList.remove("showModal");
+  }
+};
