3aa484cdd84ab105ec9dd59d4be0977dca5843a3
Mahdi <mahdiayyad74@yahoo.com>
2021-05-10 20:08:22 +0300

    Final Changes

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://mahdiayyad97:Mahdi1997@test-cluster1.urkm0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const nodemailer = require("nodemailer");
-
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 const PORT = process.env.PORT || 5000;
 
 // Middleware
@@ -40,6 +44,52 @@ app.post("/", (req, res) => {
     }
   });
 });
+
+// Passport config
+require('./config/passport')(passport);
+
+// DB config
+const db = require("./config/keys").MongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// BodyParser
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
